562efa9c1dfb75a2d76a40a1d67a232051949687
Jose Ballesteros <josebp0107@gmail.com>
2021-04-24 18:16:30 -0500

    finish to add a client to csv file by means to cli

--- sales/clients/commands.py
+++ sales/clients/commands.py
@@ -1,13 +1,23 @@
 import click
 
+from clients.services import ClientService
+from clients.models import Client 
+
 @click.group() # Convierte la funcion client en otro decorador
 def clients():
     """ Manages the clients lifecycle"""
 
 @clients.command()
+@click.option('-n', '--name',type=str, prompt=True, help= 'The client name')
+@click.option('-c', '--company',type=str, prompt=True, help= 'The client company')
+@click.option('-e', '--email',type=str, prompt=True, help= 'The client email')
+@click.option('-p', '--position',type=str, prompt=True, help= 'The client position')
 @click.pass_context
 def create(ctx, name, company, email, position):
     """Creates a new client"""
+    client = Client(name, company, email, position)
+    client_service = ClientService(ctx.obj['clients_table'])
+    client_service.create_client(client)
 
 @clients.command()
 @click.pass_context
