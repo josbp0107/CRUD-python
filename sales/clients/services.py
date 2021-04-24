6e5aab1d573b1843c618485e46a9a2e6bf6e42f7
Jose Ballesteros <josebp0107@gmail.com>
2021-04-24 18:34:56 -0500

    add method to read csv file

--- sales/clients/services.py
+++ sales/clients/services.py
@@ -10,3 +10,10 @@ class ClientService:
         with open(self.table_name, 'a', encoding='utf-8') as f:
             writer = csv.DictWriter(f, fieldnames=Client.schema())
             writer.writerow(client.to_dict())
+    
+    def list_clients(self):
+        with open('../.clients.csv', 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f, fieldnames=Client.schema())
+            # reader al ser un iterable, es necesario convertirlo a una lista con la funcion global list
+            return list(reader)
+    
