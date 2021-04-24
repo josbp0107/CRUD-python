20200de8e6200510f080658c6832f86eaf8d21c9
Jose Ballesteros <josebp0107@gmail.com>
2021-04-24 18:15:17 -0500

    add decorator staticmethod to schema method

---
+++ sales/clients/models.py
@@ -0,0 +1,19 @@
+import uuid
+
+class Client:
+    def __init__(self,name,company,email,position, uid=None):
+        self.name = name
+        self.company = company
+        self.email = email
+        self.position = position
+        self.uid = uid or uuid.uuid4()
+    
+    def to_dict(self):
+        return vars(self)
+    
+    @staticmethod
+    # nos permite declarar métodos estáticos en nuestra clase.
+    # Es un método que se puede ejecutar sin necesidad de una instancia de una clase. 
+    # No hace falta que reciba self como parámetro
+    def schema():
+        return ['name', 'company', 'email', 'position', 'uid']
